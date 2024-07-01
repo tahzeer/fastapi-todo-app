28ecb61719d77d7b40857809c151171fe994895d
Tahzeer Ashraf <63439640+tahzeer@users.noreply.github.com>
2024-07-01 12:02:38 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+POSTGRES_DATABASE_URL = 'your url'
+
+engine = create_engine(POSTGRES_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind = engine)
+
+Base = declarative_base()
